6e3992cfd3984c85ecd47d73b17ad11b4bc918cb
Vivek reddy <63183708+Vivekreddy12345@users.noreply.github.com>
2023-04-10 23:08:38 -0500

    Update Calctest.java

--- src/test/java/Calcpackage/Calctest.java
+++ src/test/java/Calcpackage/Calctest.java
@@ -25,17 +25,9 @@ public class Calctest {
 	    public void testAddition() {
 	        // Create a mock object for the Calculator class
 	        Calculator calculator = mock(Calculator.class);
-	        
-	        // Set up the mock object to return a specific value
 	        when(calculator.add(2, 3)).thenReturn(5);
-	        
-	        // Call the method under test
 	        int result = calculator.add(2, 3);
-	        
-	        // Verify that the mock object was called
 	        verify(calculator).add(2, 3);
-	        
-	        // Verify the result of the method under test
-	        assertEquals("Unexpected result", 5, result);
+	         assertEquals("Unexpected result", 5, result);
 	    }
 }
